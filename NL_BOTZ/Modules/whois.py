f9e6b05f7f2b4c43178c44c7749cc22d24049498
Muhammed Nihaal <99305409+Nihaal-TG@users.noreply.github.com>
2022-08-06 21:48:43 +0530

    Update whois.py

--- NL_BOTZ/Modules/whois.py
+++ NL_BOTZ/Modules/whois.py
@@ -10,9 +10,9 @@ from datetime import datetime
 from pyrogram import Client, filters
 from pyrogram.errors import UserNotParticipant
 from info import COMMAND_HAND_LER
-from plugins.helper_functions.extract_user import extract_user
-from plugins.helper_functions.cust_p_filters import f_onw_fliter
-from plugins.helper_functions.last_online_hlpr import last_online
+from NL_BOTZ.helper_functions.extract_user import extract_user
+from NL_BOTZ.helper_functions.cust_p_filters import f_onw_fliter
+from NL_BOTZ.helper_functions.last_online_hlpr import last_online
 
 
 @Client.on_message(
