5b18db6ce0636116e029c1eabcae72a4b0cef4be
Muhammed Nihaal <99305409+Nihaal-TG@users.noreply.github.com>
2022-08-06 21:52:42 +0530

    Update arrow.py

--- NL_BOTZ/Modules/arrow.py
+++ NL_BOTZ/Modules/arrow.py
@@ -3,7 +3,7 @@
 
 from pyrogram import Client, filters
 from info import COMMAND_HAND_LER
-from plugins.helper_functions.cust_p_filters import f_onw_fliter
+from NL_BOTZ.helper_functions.cust_p_filters import f_onw_fliter
 
 # EMOJI CONSTANTS
 DART_E_MOJI = "🎯"
