c367a79f0d47861bf83cd8837184db262c1c4610
Muhammed Nihaal <99305409+Nihaal-TG@users.noreply.github.com>
2022-08-20 14:57:55 +0530

    Update pm_filter.py

--- NL_BOTZ/pm_filter.py
+++ NL_BOTZ/pm_filter.py
@@ -821,15 +821,6 @@ async def cb_handler(client: Client, query: CallbackQuery):
         reply_markup = InlineKeyboardMarkup(buttons)
         await query.message.delete()
         if not PICS:
-            await query.message.reply_text(
-                text=script.START_TXT.format(
-                    query.from_user.mention, 
-                    temp.U_NAME, 
-                    temp.B_NAME,
-                ),
-                reply_markup=reply_markup
-            )
-        else:
             await query.message.reply_photo(
                 photo=PICS,
                 caption=script.START_TXT.format(
